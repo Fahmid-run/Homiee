5dfb35ccf50c57f2f3433287b305966411ae3f79
Fahmid-run <fahmiduddinnabil6064@gmail.com>
2026-09-15 21:37:14 +0600

    fixed send response util

--- src/utils/sendResponse.ts
+++ src/utils/sendResponse.ts
@@ -1,26 +1,30 @@
 import { Response } from "express";
 
 type TMeta = {
-    page: number;
-    limit: number;
-    total: number;
-    totalPages: number;
-}
+  page: number;
+  limit: number;
+  total: number;
+  totalPages: number;
+};
 
 type TResponseData<T> = {
-    success: boolean;
-    statusCode: number;
-    message: string;
-    data: T;
-    meta?: TMeta
-}
+  success: boolean;
+  statusCode?: number;
+  message: string;
+  data: T;
+  meta?: TMeta;
+};
 
 export const sendResponse = <T>(res: Response, data: TResponseData<T>) => {
-    res.status(data.statusCode).json({
-        success: data.success,
-        statusCode: data.statusCode,
-        message: data.message,
-        data: data.data,
-        meta: data.meta
-    })
-}
+  const validStatusCode =
+    typeof data.statusCode === "number" && Number.isInteger(data.statusCode)
+      ? data.statusCode
+      : 200;
+  res.status(validStatusCode).json({
+    success: data.success,
+    statusCode: data.statusCode,
+    message: data.message,
+    data: data.data,
+    meta: data.meta,
+  });
+};
