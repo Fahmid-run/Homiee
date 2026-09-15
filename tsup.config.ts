a1999d3f447b259171f86a32e968892bb3c1c333
Fahmid-run <fahmiduddinnabil6064@gmail.com>
2026-09-15 20:52:00 +0600

    update tsup config

--- tsup.config.ts
+++ tsup.config.ts
@@ -9,4 +9,8 @@ export default defineConfig({
   bundle: true,
   splitting: false,
   sourcemap: true,
+  banner: {
+    // Shim required for Node/Express CJS dependencies bundled in ESM
+    js: `import { createRequire } from 'module'; const require = createRequire(import.meta.url);`,
+  },
 });
