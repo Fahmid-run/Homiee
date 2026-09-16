c2a5835812d670d249e248a26ada3a47c5aca4a7
Fahmid-run <fahmiduddinnabil6064@gmail.com>
2026-09-16 15:55:43 +0600

    fix:seed admin data

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -33,7 +33,7 @@ async function main() {
 
 main()
   .catch((e) => {
-    console.error("❌ Error seeding database:", e);
+    console.error(" Error seeding database:", e);
     process.exit(1);
   })
   .finally(async () => {
